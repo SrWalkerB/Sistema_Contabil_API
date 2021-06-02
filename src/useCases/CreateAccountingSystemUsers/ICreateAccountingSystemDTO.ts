d2e1315441eba2f46a304597b0dcf9e5d1500546
srwalkerb <srwalkerbdevs@gmail.com>
2021-06-02 14:13:48 -0300

    feat: create interface CreateAccountingSystem

--- src/useCases/CreateAccountingSystemUsers/ICreateAccountingSystemDTO.ts
+++ src/useCases/CreateAccountingSystemUsers/ICreateAccountingSystemDTO.ts
@@ -1,1 +1,33 @@
+interface ICreateAccountingSystemClient{
+    name: string,
+    username: string,
+    cpf: number,
+    email: string,
+    password: string
+}
 
+interface ICreateAccountingSystemCompany{
+    name: string,
+    cnpj: number,
+    plane: string
+}
+
+interface ICreateAccountAddress{
+    cep: number,
+    road: string,
+    number: string,
+    district: string,
+    city: string,
+    state: string,
+    country: string
+}
+
+interface ICreateAccountingSystemDTO{
+    createAccountingClient: ICreateAccountingSystemClient,
+    createAccountingCompany: ICreateAccountingSystemCompany,
+    createAccountingAddress: ICreateAccountAddress
+}
+
+export {
+  ICreateAccountingSystemDTO
+}
